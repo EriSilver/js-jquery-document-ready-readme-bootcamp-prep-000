73ad90bf1420db3c7d0ce8be12dcbd8b861249d4
Erini George <dr.erinigeorge@yahoo.com>
2019-03-05 23:46:30 +0000

    Automatically backed up by Learn

--- js/script.js
+++ js/script.js
@@ -1,4 +1,9 @@
+
+
 var x = getElementById("js");
 x.src = "http://ajax.googleapis.com/ajax/libs/jquery/3.0.0/jquery.min.js";
 
-$("#text").append("this is so freaking cool.");
+$(document).ready(function ()
+{
+  $("#text").append("this is so freaking cool.");
+})
